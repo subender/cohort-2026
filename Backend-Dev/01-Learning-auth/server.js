be154e0559adebe107afd34393edee822af968c9
Subender <subender.io@outlook.com>
2026-05-31 21:10:01 +0530

    connected mongo db and env setup

---
+++ Backend-Dev/01-Learning-auth/server.js
@@ -0,0 +1,18 @@
+import "dotenv/config"
+import connectDB from "./src/common/config/db.js"
+import app from "./src/app.js"
+
+const PORT = process.env.PORT || 5000
+
+const start = async ()=>{
+
+    await connectDB()
+        app.listen(PORT, ()=>{
+            console.log(`Server is running at ${PORT} in ${process.env.NODE_ENV} mode.`)
+        })
+}
+
+start().catch((err)=>{
+    console.log("Failed to start Server", err)
+    process.exit(1)
+})
