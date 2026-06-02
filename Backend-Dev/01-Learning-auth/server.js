29137ba406c6daa90e7f56cd7e0398ade58575c3
Subender <subender.io@outlook.com>
2026-06-02 09:59:56 +0530

    created standard response class

--- Backend-Dev/01-Learning-auth/server.js
+++ Backend-Dev/01-Learning-auth/server.js
@@ -1,18 +1,19 @@
-import "dotenv/config"
-import connectDB from "./src/common/config/db.js"
-import app from "./src/app.js"
+import "dotenv/config";
+import connectDB from "./src/common/config/db.js";
+import app from "./src/app.js";
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-const start = async ()=>{
+const start = async () => {
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log(
+      `Server is running at ${PORT} in ${process.env.NODE_ENV} mode.`
+    );
+  });
+};
 
-    await connectDB()
-        app.listen(PORT, ()=>{
-            console.log(`Server is running at ${PORT} in ${process.env.NODE_ENV} mode.`)
-        })
-}
-
-start().catch((err)=>{
-    console.log("Failed to start Server", err)
-    process.exit(1)
-})
+start().catch((err) => {
+  console.log("Failed to start Server", err);
+  process.exit(1);
+});
