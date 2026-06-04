f18acd064bc73b8d2bfc92200772612d6f08592d
Subender <subender.io@outlook.com>
2026-06-04 12:38:41 +0530

    added Register DTO class

---
+++ Backend-Dev/01-Learning-auth/src/modules/auth/dto/register.dto.js
@@ -0,0 +1,15 @@
+import Joi from 'joi'
+import BaseDto from '../../../common/dto/base.dto'
+
+class RegisterDto extends BaseDto {
+    static schema = Joi.object({
+        name: Joi.string().trim().min(2).max(50).required(),
+        email: Joi.string().email().lowercase().required(),
+        password :Joi.string().min(8).pattern(/(?=.*[A-Z])(?=.*\d)/)
+        .message("password must contain at least one uppercase letter and one digit",).required(),
+        role: Joi.string().valid("customer", "seller").default("customer"),
+
+    })
+}
+
+export default RegisterDto
