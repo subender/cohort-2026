7a67cc20a1610a2b0ba3c5fdd1d0be687d325fa3
Subender <subender.io@outlook.com>
2026-06-04 12:57:42 +0530

    setup auth route and undefined routes

--- Backend-Dev/01-Learning-auth/src/app.js
+++ Backend-Dev/01-Learning-auth/src/app.js
@@ -1,8 +1,16 @@
 import express, { json } from 'express'
+import authRouter from './modules/auth/auth.routers.js'
+import ApiError from "./common/utils/api-error.js";
 
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use("/api/auth", authRouter)
+
+app.all("{*path}", (req,res)=>{
+    throw ApiError.notfound(`Route ${req.originalUrl} not found`)
+})
 
 export default app;
 
